533c21bfebf27491796e20df514680987e15e3f1
Alex de la Iglesia <alexander.delaiglesia@gmail.com>
2021-09-08 23:13:59 -0400

    Update navbar.js
    Added link to MTG Sim

--- navbar.js
+++ navbar.js
@@ -38,6 +38,7 @@ function run(file, gotoRoot) {
               <li><a href="https://projects.alexanderdelaiglesia.com/Hangman/">Hangman</a></li>
               <li><a href="https://projects.alexanderdelaiglesia.com/Hangman-Multiplayer/">Hangman Multiplayer</a></li>
               <li><a href="https://graphvideo-1.delaminer.repl.co/">GraphVideo</a></li>
+              <li><a href="https://projects.alexanderdelaiglesia.com/Magic-the-Gathering-Simulator/magic.html">Magic the Gathering Simulator</a></li>
               <li role="separator" class="divider"></li>`
               // <li><a href="` + ((gotoRoot)? '../':'') + `physics.html">2D Physics</a></li>
               // <li><a href="` + ((gotoRoot)? '../':'') + `WebGL/webgame.html">Web Game</a></li>
